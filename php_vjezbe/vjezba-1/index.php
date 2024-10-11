d6ad71b1aecac4b2aaa2ece4326075a9ae94249d
AnaBenacic <ana.benacic@proton.me>
2024-10-11 14:52:55 +0200

    Add files via upload

---
+++ php_vjezbe/vjezba-1/index.php
@@ -0,0 +1,24 @@
+<?php
+
+print'
+<!DOCTYPE html>
+<html lang="hr">
+  <head>
+    <title>Da Vincijev kod</title>
+    <meta http-equiv="content-type" content="text-html; charset=UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Ana Benačić">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  </head>
+  <body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <a href="https://hr.wikipedia.org/wiki/Da_Vincijev_kod" target="_blank">Poveznica</a>
+  </body>
+</html>
+'
+// vjezba-1
+
+?>
