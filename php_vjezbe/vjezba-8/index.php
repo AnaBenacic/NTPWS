855f5b5a7ca17df92536ec89ebe8652c6f00e9c3
AnaBenacic <ana.benacic@proton.me>
2024-10-22 14:26:58 +0200

    Add files via upload

---
+++ php_vjezbe/vjezba-8/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="hr">
+  <head>
+    <title>Vozila</title>
+    <meta http-equiv="content-type" content="text-html; charset=UTF-8">
+    <meta name="description" content="vozila">
+    <meta name="keywords" content="vozila, Audi, BMW, Renault, Citroen">
+    <meta name="author" content="Ana Benačić">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <form action="" method="post">
+      <label for="vozila">Označi vozilo:</label><br>
+      <br>
+      <input type="radio" name="vozila" value="Audi">Audi<br>
+      <input type="radio" name="vozila" value="BMW">BMW<br>
+      <input type="radio" name="vozila" value="Renault">Renault<br>
+      <input type="radio" name="vozila" value="Citroen">Citroen<br>
+      <br>
+      <input type="submit" value="Pošalji"><br>
+    </form>
+    <?php 
+      if (isset($_POST['vozila'])) {
+        $odabranoVozilo = $_POST['vozila'];
+        print '<p>Odabrano je vozilo ' . $odabranoVozilo . '.</p>';
+      }
+    ?>
+  </body>
+</html>
+
+
+<!-- vjezba-8 -->
