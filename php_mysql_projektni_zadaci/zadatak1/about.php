9dbe9401fcef5b71fde58a18f3d13c759e88a7fd
AnaBenacic <ana.benacic@proton.me>
2024-10-30 16:19:32 +0100

    Add files via upload

---
+++ php_mysql_projektni_zadaci/zadatak1/about.php
@@ -0,0 +1,21 @@
+<?php 
+
+print'
+		<h1>About</h1>
+		<h2>Who is behind this blog?</h2>
+		<video width="50%" controls poster="img/video-cover-sir-tim-berners-lee.jpg" >
+		  <source src="video/art.mp4" type="video/mp4">
+		  Your browser does not support HTML5 video.
+		</video>
+		<p>My name is Ana Benačić and I am greatly interested in all kinds of art.</p>
+		<p>I was born on January 1st, 1970, in Zagreb, Croatia. From a young age, I started dabbling in the arts. In kindergarden, I started drawing like a little kid does. When I was only 6 years old, I learned how to read. That was a shocker to my mom!</p>
+		<p>In elementary school things were going steady. I visited the library every Saturday, where I did a lot of different art activities. Clay, collage, painting, you name it!</p>
+		<p>By the end of elementary school I wrote some theatre plays and played in them. Around that time I also joined an actual theatre group in my town\'s theatre, but I wasn\'t in it for long.</p>
+		<p>I also started writing in my free time around the end of elementary school. Oh, how I miss those times, because my brain was off and I could just write about anything and everything, in pure kid form.</p>
+		<p>In high school I mostly drew in my school notebooks, as the free time I had was now filled with school work. Who thought 16 subjects at once was a good idea?</p>
+		<p>Ever since I started college, I slowly picked up on a few of different art hobbies, such as cross stitching, crocheting and diamond painting.</p>
+		<p>You are now pretty much up to date with who I am and what I like. If you have any questions, please head to the contact page!</p>
+		<p>Oh, up there is a video that I really liked, give it a watch! It was made by <a href="https://www.youtube.com/@GCFLearnFree" target="_blank">LearnFree on YouTube</a>.</p>
+		'
+
+?>
