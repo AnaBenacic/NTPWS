d5801bbdfbbfd3f2213deccbfdbc144ff36cd2ff
AnaBenacic <ana.benacic@proton.me>
2024-11-02 15:18:52 +0100

    Add files via upload

---
+++ php_vjezbe/vjezba-14/users.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 02, 2024 at 02:14 PM
+-- Server version: 8.0.39-0ubuntu0.24.04.2
+-- PHP Version: 8.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vjezba-14`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `country_code` varchar(3) NOT NULL,
+  `about` text,
+  `created` tinyint UNSIGNED NOT NULL,
+  `modified` tinyint UNSIGNED NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `lastname`, `username`, `password`, `country_code`, `about`, `created`, `modified`, `date`) VALUES
+(1, 'Ana', 'Benačić', 'admin', 'admin_123!', 'HR', 'admin stranice', 1, 0, '2024-11-02 13:58:57'),
+(2, 'Marko', 'Markić', 'markom', 'markom_123!', 'HR', 'adminov brat', 1, 0, '2024-11-02 13:59:04'),
+(3, 'Ivo', 'Ivić', 'IvoI', 'ivoi_123!', 'GB', 'adminov prijatelj', 1, 0, '2024-11-02 13:59:09'),
+(4, 'Sanja', 'Sanjić', 'SanjaS', 'sanjas_123!', 'US', 'adminova prijateljica', 1, 0, '2024-11-02 14:00:18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_key` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
